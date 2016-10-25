0c98ca4a5dd586b8a5be3f05cf692e929f5857bc
Anna <samslik@gmail.com>
2016-10-25 22:58:44 +0200

    solved

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,30 @@
 #write your code here
+def echo(words)
+  words
+end
+
+def shout(repeat)
+  repeat.upcase
+end
+
+def repeat(repeat,repNumb = 2)
+  (repeat + ' ') * (repNumb - 1) + repeat
+end
+
+def start_of_word(stWord, lettersNumber)
+  i = 0
+  start = ''
+  while i < lettersNumber do
+  start = start + stWord[i]
+  i+=1
+  end
+  start
+end
+
+def first_word (str)
+  str.split[0]
+end
+
+def titleize (words)
+  words.split.map {|n| n.capitalize}.join(' ')
+end
