4ca0eb432add9a9538ee0f06ea44237d4efd9f76
Anna <samslik@gmail.com>
2016-10-25 11:30:45 +0200

    created

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,1 +1,10 @@
 #write your code here
+def hello
+hello = "Hello!"
+end
+
+def greet(someone)
+  greet = "Hello, #{someone}!"
+end
+
+greet("Alice")
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,13 @@
 #write your code here
+sum = []
+def add (a,b)
+  add = a + b
+end
+
+def subtract (a,b)
+  subtract = a - b
+end
+
+def sum
+  sum.push[add]
+end
